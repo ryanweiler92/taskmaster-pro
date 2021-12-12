3b4a9323f58c66a5598777105690ba0d4d4a9b6a
ryanweiler92 <ryanweiler92@gmail.com>
2021-12-12 18:08:23 -0500

    added moment time features

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,34 @@
 var tasks = {};
 
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+
+  //convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  //remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  //apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+
+  //apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+
+};
+
+$("#modalDueDate").datepicker({
+  minDate: 1
+});
+
 $(".card .list-group").sortable({
   connectWith: $(".card .list-group"),
   scroll: false,
@@ -67,9 +96,11 @@ $("#trash").droppable({
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
+
   var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(taskDate);
+
   var taskP = $("<p>")
     .addClass("m-1")
     .text(taskText);
@@ -77,6 +108,7 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -166,11 +198,18 @@ $(".list-group").on("click", "span", function() {
   //swap out elements
   $(this).replaceWith(dateInput);
 
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      $(this).trigger("change");
+    }
+  });
+
   //alutomatically focus on new element
   dateInput.trigger("focus");
 });
 
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   //get current text
   var date = $(this)
   .val()
@@ -192,12 +231,14 @@ $(".list-group").on("blur", "input[type='text']", function() {
   saveTasks();
 
   //recreate span element with bootstrap classes
-  var taskSPan = $("<span>")
+  var taskSpan = $("<span>")
   .addClass("badge badge-primary badge-pill")
   .text(date);
 
   //replace in put with span element
   $(this).replaceWith(taskSpan);
+
+  auditTask;uditTask($(taskSpan).closest(".list-group-item"));
 })
 
 
